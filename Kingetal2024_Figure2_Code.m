534b60f757ee7d8398b52958bf4c1b30d33572d9
Meghan King <101216124+meghan-king@users.noreply.github.com>
2024-11-27 20:30:23 -0800

    Add files via upload

---
+++ Kingetal2024_Figure2_Code.m
@@ -0,0 +1,42 @@
+%% Figure 2 
+%Code to plot Figure 2 in King et al., 2024. 
+
+Input_data = importdata('Kingetal2024_IceModel_Inputd18O.txt'); %Input raw Lisiecki & Raymo d18O data
+
+Time = flip(Input_data(:,1)); %Create Time vector from input
+d18O = flip(Input_data(:,2)); %Create d18O vector from input
+
+neg_d18O = (d18O)*-1; %Get the negative of the d18O curve
+min_d18O = min(neg_d18O); %Create min d18O vector
+max_d18O = max(neg_d18O); %Create max d18) vector
+Scalar_factor_initial = zeros(length(Time),1); %Create scalar_factor_intitial vector and fill with Time in first column and 0s in 2nd
+
+for i = 1:length(Time) %For the length of the Time vector
+    Scalar_factor_initial(i) = (neg_d18O(i)-min_d18O)/(max_d18O-min_d18O); %Normalize d18O curve 
+end
+
+
+%Figure 1a
+subplot(2,1,1)
+plot(Time,d18O,'color','k','LineWidth',1)
+xlim([2900 3650])
+ylim([2.8 3.8])
+a = get(gca,'XTickLabel');
+set(gca,'XTickLabel',a,'fontsize',12)
+set(gcf,'color','w')
+set(gca,'XDir','reverse')
+set(gca,'YDir','reverse')
+% xlabel('Time (ka)','FontSize',16)
+ylabel('\delta^1^8O (?)','FontSize',16)
+
+%Figure 1b
+subplot(2,1,2)
+plot(Time,Scalar_factor_initial,'color','k','LineWidth',1)
+xlim([2900 3650])
+ylim([-0.1 1.2])
+a = get(gca,'XTickLabel');
+set(gca,'XTickLabel',a,'fontsize',12)
+set(gcf,'color','w')
+set(gca,'XDir','reverse')
+xlabel('Time (ka)','FontSize',16)
+ylabel('Normalized Stack','FontSize',16)
